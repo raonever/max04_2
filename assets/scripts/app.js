a4f84e887b53979e43eab377fe227f4ed884fc51
raonever <ever29lkh@gmail.com>
2020-10-01 01:43:05 +0900

    sub 85. won or lost

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,24 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
-let chosenMaxLife = 100;
+let chosenMaxLife = 4;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-// 몬스터에게 주는 데미지
 function attackHandler() {
     const damage = dealMonsterDamage(ATTACK_VALUE);
     currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('You have a draw!')
+    }
 }
 
 attackBtn.addEventListener('click', attackHandler);
